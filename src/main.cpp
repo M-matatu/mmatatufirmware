fec6fdcd463786391919256293fe19be35f2ae2d
franfreezy <frandelwanjawa@yahoo.com>
2024-11-19 12:47:54 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,32 @@
 #include "gps.h"
 #include "lcd.h"
 #include "comms.h"
-void setup() {
-   
+void setup()
+{
+
   pinMode(passenger1_PIN, INPUT);
   pinMode(passenger2_PIN, INPUT);
-  Serial.begin(9600);    
-  gpsSerial.begin(9600); 
-  SPI.begin();        
+  Serial.begin(9600);
+  gpsSerial.begin(9600);
+  SPI.begin();
   rfid.PCD_Init();
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   // lcd
-  lcd.begin();
-    lcd.backlight(); 
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("LCD Ready...");
-    delay(2000); 
-    lcd.clear();
-
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("LCD Ready...");
+  delay(2000);
+  lcd.clear();
 }
 
-void loop() {
-  String state = getTouchState(); 
-  displayMessage("message" );
-  Serial.println(state);          
-  delay(500);   
+void loop()
+{
+  String state = getTouchState();
+  displayMessage("message");
+  Serial.println(state);
+  delay(500);
   String coordinates = getCoordinates();
 }
-
